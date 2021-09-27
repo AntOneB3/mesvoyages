f24bc936ed8efaf6dee5ab019a83a9b706e94caa
abego <abegou@gmail.com>
2021-09-27 17:40:04 +0200

    Phase 8 : ORM (affichage en colonne des informations provenant de la BDD).

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -8,6 +8,7 @@
 
 namespace App\Controller;
 
+use App\Repository\VisiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -25,8 +26,26 @@ class VoyagesController extends AbstractController{
      */
     public function index() : Response 
     {
-        return $this->render("pages/voyages.html.twig");
-    }    
-
+        $visites = $this->repository->findAll();
+        return $this->render("pages/voyages.html.twig", ['visites' => $visites]);
+        
+        
+    }   
     
+    /**
+     *
+     * @var type 
+     */
+    private $repository;
+    
+    /**
+     * 
+     * @param VisiteRepository $repository
+     */
+    function __construct(VisiteRepository $repository) {
+        $this->repository = $repository;
+    }
+    
+    
+
 }
--- src/Repository/VisiteRepository.php
+++ src/Repository/VisiteRepository.php
@@ -14,6 +14,7 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class VisiteRepository extends ServiceEntityRepository
 {
+    
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Visite::class);
